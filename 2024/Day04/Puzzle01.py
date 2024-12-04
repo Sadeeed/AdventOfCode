18d0cd6785fdb67e6f90093d99b2d3dde7451394
Sadeeed <sadeeedw@gmail.com>
2024-12-05 02:16:20 +0500

    📦add(2024): Day04 P1

--- 2024/Day04/Puzzle01.py
+++ 2024/Day04/Puzzle01.py
@@ -1,7 +1,8 @@
 from utils import get_input
 import re
+import numpy as np
 
-input = get_input('test_input.txt') # rows
+input = get_input('input.txt') # rows
 
 def get_columns(input):
     columns = []
@@ -13,6 +14,15 @@ def get_columns(input):
                 columns[i] += char
     return columns
 
+def get_columns_np(input):
+    # list comprehension to spit each element in input to a list
+    input = [list(row) for row in input]
+    input = np.array(input)
+    # input = np.transpose(input)
+    # return [''.join(row) for row in input]
+    return ["".join(input[:, i]) for i in range(input.shape[1])]
+
+
 def get_diagonals(input):
     diagonals = []
     for i, row in enumerate(input):
@@ -23,24 +33,35 @@ def get_diagonals(input):
                 diagonals[i + j] += char
     return diagonals
 
+
+def get_diagonals_np(input):
+    # list comprehension to spit each element in input to a list
+    input = [list(row) for row in input]
+    input = np.array(input)
+
+    list_ = [input[::-1,:].diagonal(i) for i in range(-input.shape[0]+1,input.shape[1])]
+    list_.extend(input.diagonal(i) for i in range(input.shape[1]-1,-input.shape[0],-1))
+
+    # input = np.flipud(input)
+    # a = input.shape[0]
+    # list_ = [np.diag(input, k=i).tolist() for i in range(-a+1,a)]
+    return [''.join(row) for row in list_]
+
 columns = get_columns(input)
-diagonals = get_diagonals(input)
+# diagonals = get_diagonals(input)
+diagonals = get_diagonals_np(input)
+
+
 
 total = 0
-
 print("Rows\n")
 for row in input:
     row_results = re.findall(r'XMAS', row)
     row_results_i = re.findall(r'XMAS', row[::-1])
-    # row_results_i = re.findall(r'SAMX', row)
-    print(f"Row results: {row_results}")
-    print(f"Row results_i: {row_results_i}")
     # print(f"Row results: {row_results}")
     total += len(row_results) + len(row_results_i)
     print(row)
-    print(row[::-1])
 print("\n************\n")
-
 
 
 print("Columns\n")
@@ -58,8 +79,6 @@ print("Diagonals\n")
 for diagonal in diagonals:
     diagonal_results = re.findall(r'XMAS', diagonal)
     diagonal_results_i = re.findall(r'XMAS', diagonal[::-1])
-    # diagonal_results_i = re.findall(r'SAMX', diagonal)
-    # print(f"Diagonal results: {diagonal_results}")
     total += len(diagonal_results) + len(diagonal_results_i)
     print(diagonal)
 print("\n************")
